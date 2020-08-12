ffa6f0a13fb7b4a967c614f5a39103ad10f8539e
akt-2019 <akt_2018@outlook.com>
2020-08-12 16:58:53 +0900

    Added second hand of clock.

--- js/clock.js
+++ js/clock.js
@@ -1,29 +1,55 @@
 "use strict";
 var canvas = document.getElementById("clock-canvas");
 var ctx = canvas.getContext("2d");
-var currentPos;
-var currentRad = getRadian(270);
-var radInterval = getRadian(6);
-ctx.setLineDash([3, 2]);
-currentPos = [256, 0];
-ctx.moveTo(256, 256);
-ctx.lineTo(currentPos[0], currentPos[1]);
-ctx.stroke();
-setInterval(function () {
-    ctx.moveTo(0, 0);
-    ctx.clearRect(0, 0, 512, 512);
-    var newRad = currentRad - radInterval;
-    var newPos = [
-        -256 * Math.cos(newRad) + 256,
-        256 * Math.sin(newRad) + 256
-    ];
-    ctx.moveTo(256, 256);
-    ctx.lineTo(newPos[0], newPos[1]);
+var posSecond;
+var posMinute;
+var intervalSecond = getRadian(6);
+var intervalMinute = getRadian(30);
+var lenSecond = 200;
+var lenMinute = 185;
+posSecond = {
+    x: 0,
+    y: lenSecond
+};
+posMinute = {
+    x: 0,
+    y: lenMinute
+};
+var tmrSecond = setInterval(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "#ff0000";
+    var newPos = {
+        x: posSecond.x * Math.cos(intervalSecond) + posSecond.y * Math.sin(intervalSecond),
+        y: posSecond.y * Math.cos(intervalSecond) - posSecond.x * Math.sin(intervalSecond)
+    };
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, canvas.height / 2);
+    ctx.lineTo(convertX(newPos.x), convertY(newPos.y));
+    ctx.closePath();
     ctx.stroke();
-    currentRad = newRad;
-    currentPos = newPos;
+    posSecond = newPos;
 }, 1000);
-function getRadian(d) {
-    return (Math.PI * d) / 180;
+var tmrMinute = setInterval(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "#00ff00";
+    var newPos = {
+        x: posSecond.x * Math.cos(intervalMinute) + posSecond.y * Math.sin(intervalMinute),
+        y: posSecond.y * Math.cos(intervalMinute) - posSecond.x * Math.sin(intervalMinute)
+    };
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, canvas.height / 2);
+    ctx.lineTo(convertX(newPos.x), convertY(newPos.y));
+    ctx.closePath();
+    ctx.stroke();
+    posSecond = newPos;
+}, 60000);
+function getRadian(degree) {
+    return (Math.PI * degree) / 180;
+}
+function convertX(orgX) {
+    return canvas.width / 2 + orgX;
+}
+function convertY(orgY) {
+    return canvas.height / 2 - orgY;
 }
 //# sourceMappingURL=clock.js.map
--- ts/clock.ts
+++ ts/clock.ts
@@ -1,35 +1,72 @@
 let canvas:HTMLCanvasElement=document.getElementById("clock-canvas") as HTMLCanvasElement;
 let ctx=canvas.getContext("2d") as CanvasRenderingContext2D;
-let currentPos:Array<number>;
-let currentRad:number=getRadian(270);
-let radInterval:number=getRadian(6);
+let posSecond:Coordinate;
+let posMinute:Coordinate;
+let intervalSecond:number=getRadian(6);
+let intervalMinute:number=getRadian(30);
+let lenSecond=200;
+let lenMinute=185;
 
-ctx.setLineDash([3,2]);
-currentPos=[256,0];
-ctx.moveTo(256,256);
-ctx.lineTo(currentPos[0],currentPos[1]);
-ctx.stroke();
+posSecond={
+    x:0,
+    y:lenSecond
+};
 
-setInterval(function(){
-    ctx.moveTo(0,0);
-    ctx.clearRect(0,0,512,512);
-    let newRad=currentRad-radInterval;
-    let newPos=[    
-        -256*Math.cos(newRad)+256,
-        256*Math.sin(newRad)+256
-    ];
-    ctx.moveTo(256,256);
+posMinute={
+    x:0,
+    y:lenMinute
+};
+
+let tmrSecond=setInterval(function(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.strokeStyle="#ff0000";
+    let newPos:Coordinate={
+        x:posSecond.x*Math.cos(intervalSecond)+posSecond.y*Math.sin(intervalSecond),
+        y:posSecond.y*Math.cos(intervalSecond)-posSecond.x*Math.sin(intervalSecond)
+    };
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2,canvas.height/2);
     ctx.lineTo(
-        newPos[0],
-        newPos[1]
+        convertX(newPos.x),
+        convertY(newPos.y)
     );
+    ctx.closePath();
     ctx.stroke();
-    currentRad=newRad;
-    currentPos=newPos;
+    posSecond=newPos;
 
 },1000);
 
-function getRadian(d:number):number{
-    return (Math.PI*d)/180
+let tmrMinute=setInterval(function(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.strokeStyle="#00ff00";
+    let newPos:Coordinate={
+        x:posSecond.x*Math.cos(intervalMinute)+posSecond.y*Math.sin(intervalMinute),
+        y:posSecond.y*Math.cos(intervalMinute)-posSecond.x*Math.sin(intervalMinute)
+    };
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2,canvas.height/2);
+    ctx.lineTo(
+        convertX(newPos.x),
+        convertY(newPos.y)
+    );
+    ctx.closePath();
+    ctx.stroke();
+    posSecond=newPos;
+},60000);
+
+function getRadian(degree:number):number{
+    return (Math.PI*degree)/180
 }
 
+function convertX(orgX:number):number{
+    return canvas.width/2+orgX;
+}
+
+function convertY(orgY:number):number{
+    return canvas.height/2-orgY;
+}
+
+interface Coordinate{
+    x:number;
+    y:number;
+}
